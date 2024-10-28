e535dc8c9c6ce341b6c3153db11042783645d002
rami <S1749820@online.houstonisd.org>
2024-10-27 23:39:26 -0500

    final update

--- src/cory.java
+++ src/cory.java
@@ -15,7 +15,7 @@ public class cory extends Characters {
         super(x, y, 300, 300, 40, 20, 15, 1000, new ImageIcon("C:\\Users\\Demon\\Desktop\\gannee\\rpg-gamee\\images\\cory.png"));
     }
     public String toString(){
-        return "cory"+ super.getHea() + " " + super.getDam() + " " + super.getStam()+" " + super.getSp();
+        return "cory"+ super.getHea() + " " + super.getDam() + " " + super.getStam()+" " + super.getSp()+ ""+super.getWeapon();
     }
     public void drawWeapon(Graphics2D g, int x, int y){
         if (weapon instanceof twinblade){
